6df1809df71265ff5908d1cbe2d2c377d27346ab
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-04-12 01:36:54 +0300

    create variable in the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 function akanName(){
-    
+    var yr=document.getElementById("year").value;
+    var cc=parseInt(yr.slice(0 ,1))
+    var yy =parseInt(yr.slice(2 ,3))
+    var mm = parseInt(document.getElementById("month").value);
+    var dd = parseInt(document.getElementById("day").value);
+    var gender = document.getElementByName("gender")
 }
