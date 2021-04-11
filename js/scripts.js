12cedee2b5c110927987d3718776bb6b7b9ee195
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-04-12 01:56:14 +0300

    use the if statement to get the day of the week and the associated aakn name

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,40 @@ function akanName(){
     var femaleName=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
     var birthday=(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)
     var getAkanName;
+    if(birthday%7==0 && gender.checked==male){
+        alert("You were born on "+ dayWeek[0] +" and your Akna name is "+maleName[0])
+    }else if (birthday%7==1 && gender.checked==male){
+      alert("You were born on "+ dayWeek[1] +" and your Akna name is "+maleName[1])
+    }else if (birthday%7==2 && gender.checked==male){
+      alert("You were born on "+ dayWeek[2] +" and your Akna name is "+maleName[2])
+    }else if (birthday%7==3 && gender.checked==male){
+      alert("You were born on "+ dayWeek[3] +" and your Akna name is "+maleName[3])
+    }else if (birthday%7==4 && gender.checked==male){
+      alert("You were born on "+ dayWeek[4] +" and your Akna name is "+maleName[4])
+    }else if (birthday%7==5 && gender.checked==male){
+      alert("You were born on "+ dayWeek[5] +" and your Akna name is "+maleName[5])
+    }else if (birthday%7==6 && gender.checked==male){
+      alert("You were born on "+ dayWeek[6] +" and your Akna name is "+maleName[6])
+    }else if (birthday%7==0 && gender.checked==female){
+      alert("You were born on "+ dayWeek[0] +" and your Akna name is "+femaleName[0])
+    }else if (birthday%7==1 && gender.checked==female){
+      alert("You were born on "+ dayWeek[1] +" and your Akna name is "+femaleName[1])
+    }else if (birthday%7==2 && gender.checked==female){
+      alert("You were born on "+ dayWeek[2] +" and your Akna name is "+demaleName[2])
+    }else if (birthday%7==3 && gender.checked==female){
+      alert("You were born on "+ dayWeek[3] +" and your Akna name is "+femaleName[3])
+    }else if (birthday%7==4 && gender.checked==female){
+      alert("You were born on "+ dayWeek[4] +" and your Akna name is "+femaleName[4])
+    }else if (birthday%7==5 && gender.checked==female){
+      alert("You were born on "+ dayWeek[5] +" and your Akna name is "+femaleName[5])
+    }else if (birthday%7==6 && gender.checked==female){
+      alert("You were born on "+ dayWeek[6] +" and your Akna name is "+femaleName[6])
+    }
+    if (year<1900 && year>2100){
+        onlcick.button; alert("Please enter a valid year")
+    }else if (month<=0 && month>12){
+        alert("please enter a valid month")
+    }else if (day<=0 && day>31){
+        alert ("please enter a valid day")
+    }
 }
