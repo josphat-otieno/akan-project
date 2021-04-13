8d5fb5df71334ead39e1d9943926a5814284c920
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-04-13 06:51:55 +0300

    provide a conditional statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 function akanName(){
-    var yr=document.getElementById("year").value;
-    var cc=parseInt(yr.slice(0 ,1))
-    var yy =parseInt(yr.slice(2 ,3))  
-    var mm = parseInt(document.getElementById("month").value);
-    var dd = parseInt(document.getElementById("day").value);
-    var dayBorn=mm+"/"+dd+"/"+yr;
-    var gender= prompt("Enter your you gender, either male or female?")
-    getAkanName(dayBorn);
-    } 
-    function getAkanName(dayBorn, gender){
-      var dateOfBirth = new Date(dayBorn);
-      var daysOfWeek = dateOfBirth.getDay();  
-      var maleName= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-      var femaleName= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
-    
-    if(year<1900 || year>2036 && month<=0 || month>12 && day<=0 || day>31){
-      alert("Please enter valid values")
+  var yr=document.getElementById("year").value;
+  var cc=parseInt(yr.slice(0 ,1))
+  var yy =parseInt(yr.slice(2 ,3))  
+  var mm = parseInt(document.getElementById("month").value);
+  var dd = parseInt(document.getElementById("day").value);
+  var dayBorn=mm+"/"+dd+"/"+yr;
+  var gender= prompt("Enter your you gender, male or female?")
+  alert("Your date of birth (mm/dd/yy) is " +(dayBorn));
+  getAkanName(dayBorn,gender);
+  // daysOfWeek=(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7
+  } 
+  function getAkanName(dayBorn, gender){
+    var dateOfBirth = new Date(dayBorn);
+    var daysOfWeek = dateOfBirth.getDay(); 
+    var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var maleName= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    var femaleName= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
+  
+  if(year<1900 || year>2037 && month<=0 || month>12 && day<=0 || day>31){
+     alert("Please enter valid values")
+  }
+    if(gender == "male"){
+       alert("Your were born on  " + weekDay[daysOfWeek] + " and your Akan name is " + maleName[daysOfWeek]);
+    }else if (gender == "female"){
+      alert("You were born on " + weekDay[daysOfWeek] + " and your Akan is "+ femaleName[daysOfWeek]);
     }
-      if(gender == "male"){
-         console.log (maleName([daysOfWeek]));
-      }else if (gender == "female"){
-        alert("You were born on " + daysOfWeek[d] + " and you were akan is "+ femaleName[daysOfWeek]);
-      }
-    }
-    
-    
-    
-    
+  }
+  
+  
